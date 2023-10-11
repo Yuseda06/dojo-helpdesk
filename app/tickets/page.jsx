e525f6dd042f67412f3cfca690656d164815d43f
Ku Muhd Yusri Ku Saad <yusryku@gmail.com>
2023-10-11 14:29:29 +0800

    lesson 7

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import TicketList from "./TicketList";
 
 export default function News() {
   return (
     <main>
-      <h2>Tickets</h2>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p>
+            <small>Currently open tickets.</small>
+          </p>
+        </div>
+      </nav>
+      <TicketList />
     </main>
   );
 }
